e0b88ffc89a1f08d2e6ea912825a84c3751fdebd
Aronsaid <aronm@hiof.no>
2026-01-14 13:24:36 +0100

    Lagt til kjøpre registeret

--- LEGOdudes/shopscript.js
+++ LEGOdudes/shopscript.js
@@ -11,10 +11,19 @@ function fetchProducts() {
                 <a href="#">${p.category}</a>
                 <h3>${p.title}</h3>
                 <p>Kr. ${p.price},-</p>
-                <button onClick="">Legg til handlevogn</button>
+                <button onClick="addToCart (${p.prodid})">Legg til handlevogn</button>
             </article>`)
 
             document.getElementById("product-list").innerHTML = productHTML
 }
 
 fetchProducts()
+
+//Legg til i handlevogn
+function addToCart(prodid) {
+    console.log("Legg til produkt med id: " + prodid)
+    cart.push(prodid)
+    console.log(cart)
+    document.getElementById("cart-quantity").innerHTML = cart.length
+
+}
